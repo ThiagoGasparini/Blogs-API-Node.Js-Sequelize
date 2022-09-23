a5912dd8418a90bdbd8fca6e758c2290a5c13579
Thiago Gasparini <thiago_engprod@outlook.com>
2022-09-23 16:10:17 -0300

    req 2 concluido

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,                                                   
+    image: DataTypes.STRING,
+  }, 
+  {
+    timestamps: false,
+    tableName: 'users',
+  });
+  return User;
+};
